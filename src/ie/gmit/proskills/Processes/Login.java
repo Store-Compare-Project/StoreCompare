0e835afb99c7ec2df1bb9a007ca74ae14d7b516b
cian2009 <g00337022@gmit.ie>
2018-03-12 13:08:46 +0000

    added MongoDB driver jar and set up login lcass for connection

--- src/ie/gmit/proskills/Processes/Login.java
+++ src/ie/gmit/proskills/Processes/Login.java
@@ -1,5 +1,13 @@
 package ie.gmit.proskills.Processes;
 
+import com.mongodb.*;
+
 public class Login {
 
+	public static void main(String username, String password) {
+		
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		
+		
+	}
 }

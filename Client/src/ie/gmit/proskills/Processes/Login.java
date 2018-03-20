18997eaef69604e52ce82725bc6636fe03334b2e
Cian Gannon <lego@live.ie>
2018-03-20 18:44:14 +0000

    Formatted Login.java

--- Client/src/ie/gmit/proskills/Processes/Login.java
+++ Client/src/ie/gmit/proskills/Processes/Login.java
@@ -6,20 +6,12 @@ import ie.gmit.proskills.object.LoginObject;
 import ie.gmit.proskills.serverconn.Requester;
 
 public class Login {
-	
-	public static Boolean loginStatus = false;
 
-	@SuppressWarnings("deprecation")
 	public static Boolean main(String username, String password) {
 			   
-	   String messageSend = "login " + username + " " + password;
+		String messageSend = "login " + username + " " + password;
 	   
-	   Requester.main(messageSend);
-	   
-	   System.out.println(loginStatus);
-			   
-		
-		
+	   	Requester.main(messageSend);
 		
 		return LoginObject.getLogin();
 	}

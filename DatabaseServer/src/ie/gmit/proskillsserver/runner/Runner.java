efac413874eb345012a65d83447266910cc76ff9
cian2009 <g00337022@gmit.ie>
2018-03-21 14:21:17 +0000

    Fixed server runner

--- DatabaseServer/src/ie/gmit/proskillsserver/runner/Runner.java
+++ DatabaseServer/src/ie/gmit/proskillsserver/runner/Runner.java
@@ -1,10 +1,12 @@
 package ie.gmit.proskillsserver.runner;
 
+import ie.gmit.proskillsserver.server.EchoServer;
+
 public class Runner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		
+		EchoServer.main(args);
 		
 	}
 	

cdde8cfb4c95e8adb02058539a32d7a0256a9b97
Cian Gannon <lego@live.ie>
2018-03-29 18:52:14 +0100

    Removed error

--- Client/src/ie/gmit/proskills/Processes/Register.java
+++ Client/src/ie/gmit/proskills/Processes/Register.java
@@ -4,11 +4,6 @@ import ie.gmit.proskills.serverconn.Requester;
 
 public class Register {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 	public static boolean main(String username, String password) {
 		
 		String messageSend = "register " + username + " " + password;

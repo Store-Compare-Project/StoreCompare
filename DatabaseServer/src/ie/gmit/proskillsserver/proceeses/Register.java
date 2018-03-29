2b8ff9b8ac6ddb0890fcf5047294aeee46532af4
Cian Gannon <lego@live.ie>
2018-03-29 18:57:28 +0100

    Initial server check done

--- DatabaseServer/src/ie/gmit/proskillsserver/proceeses/Register.java
+++ DatabaseServer/src/ie/gmit/proskillsserver/proceeses/Register.java
@@ -1,11 +1,46 @@
 package ie.gmit.proskillsserver.proceeses;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
 public class Register {
 
-	public static void main(String username, String password) {
+	@SuppressWarnings("deprecation")
+	public static Boolean main(String username, String password) {
 		
+		Boolean loginStatus = false;
 		
-
+		MongoClient mongoClient = new MongoClient("35.197.218.124", 27017);
+		DB db = mongoClient.getDB("loginproject");
+		DBCollection coll = db.getCollection("login");
+		
+		DBCursor cursor = coll.find();
+		
+		try {
+		   while(cursor.hasNext() && !loginStatus) {
+			   
+			   DBObject temp = cursor.next();
+		       
+			   String usernameTemp = temp.get("username").toString();
+			   
+			   if(usernameTemp.equals(username)){
+				   
+				   loginStatus = true;
+				   
+			   }
+			   
+			   System.out.println(loginStatus);
+			   
+		   }
+		} finally {
+		   cursor.close();
+		}
+		
+		mongoClient.close();
+		
+		return loginStatus;
 	}
-
 }

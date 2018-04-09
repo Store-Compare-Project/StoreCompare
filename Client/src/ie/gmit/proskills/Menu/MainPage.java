d817b7e822ff3b5831742d7d3c39a4e07103fa04
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-04-09 18:19:40 +0100

    Added Storage package and class from prototype

--- Client/src/ie/gmit/proskills/Menu/MainPage.java
+++ Client/src/ie/gmit/proskills/Menu/MainPage.java
@@ -1,8 +1,11 @@
 package ie.gmit.proskills.Menu;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import ie.StoreCompare.storage.Items;
 import ie.gmit.proskills.Websites.Discogs;
 
 public class MainPage 
@@ -18,6 +21,9 @@ public class MainPage
 		boolean menuKeeper = true;
 		String searchTerm = null;
 		
+		// Items Object list array
+		List<Items> itemList = new ArrayList<Items>();
+				
 		// Decelerations
 		Scanner reader = new Scanner(System.in);
 
@@ -25,6 +31,7 @@ public class MainPage
 		System.out.println("YOU ARE IN THE MAINPAGE CLASS!");
 		
 		// Prompt user for input
+		System.out.println("Enter item to search: ");
 		searchTerm = reader.nextLine();
 		
 	}
--- Client/src/ie/gmit/proskills/Websites/Discogs.java
+++ Client/src/ie/gmit/proskills/Websites/Discogs.java
@@ -1,5 +1,13 @@
 package ie.gmit.proskills.Websites;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Discogs {
 
 	

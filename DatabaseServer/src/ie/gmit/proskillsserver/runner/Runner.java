31a1bd030837f94d95b2a6066196086366890d2b
Cian Gannon <lego@live.ie>
2018-04-18 22:59:13 +0100

    Date settings on server output

--- DatabaseServer/src/ie/gmit/proskillsserver/runner/Runner.java
+++ DatabaseServer/src/ie/gmit/proskillsserver/runner/Runner.java
@@ -1,7 +1,8 @@
 package ie.gmit.proskillsserver.runner;
 
-import java.io.OutputStream;
-import java.io.PrintStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import ie.gmit.proskillsserver.server.EchoServer;
 
@@ -9,7 +10,11 @@ public class Runner {
 
 	public static void main(String[] args) throws Exception {
 		
-		System.out.println("> Starting Server...");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println(); //2016/11/16 12:08:43
+		
+		System.out.println("> (" + dateFormat.format(date) + ") Starting Server...");
 		
 		System.err.close();
 		

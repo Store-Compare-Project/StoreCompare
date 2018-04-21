43c1fa0d35725cf61b9065e8f98969936c5a2def
Cian Gannon <lego@live.ie>
2018-04-21 21:28:26 +0100

    Commented Landing page

--- Client/src/ie/gmit/proskills/Menu/LandingPage.java
+++ Client/src/ie/gmit/proskills/Menu/LandingPage.java
@@ -42,15 +42,19 @@ public class LandingPage extends JFrame {
 	 * Create the frame.
 	 */
 	public LandingPage() {
+		
+		// Frame settings
 		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 560, 420);
+		setTitle("StoreCompare - Landing Page");
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
+		// Inner panel settings
 		JPanel panel = new JPanel();
 		panel.setForeground(Color.DARK_GRAY);
 		panel.setBackground(Color.DARK_GRAY);
@@ -58,22 +62,22 @@ public class LandingPage extends JFrame {
 		contentPane.add(panel);
 		panel.setLayout(null);
 
+		// Text labels
 		JLabel welcomeLabel = new JLabel("Welcome to StoreCompare");
 		welcomeLabel.setBounds(66, 21, 410, 53);
 		welcomeLabel.setForeground(Color.LIGHT_GRAY);
 		welcomeLabel.setBackground(Color.BLACK);
 		welcomeLabel.setFont(new Font("Papyrus", Font.PLAIN, 33));
 		panel.add(welcomeLabel);
-
+		
+		// Image labels
 		JLabel imageLabel = new JLabel("");
 		Image img = new ImageIcon(this.getClass().getResource("/img/barcode.png")).getImage();
 		imageLabel.setIcon(new ImageIcon(img));
 		imageLabel.setBounds(207, 85, 128, 165);
 		panel.add(imageLabel);
 
-		JLabel imageLabel2 = new JLabel("");// Icon made by Pixel Perfect from
-											// "https://www.flaticon.com/"
-		// is licensed by "http://creativecommons.org/licenses/by/3.0/"
+		JLabel imageLabel2 = new JLabel("");
 		Image img2 = new ImageIcon(this.getClass().getResource("/img/login.png")).getImage();
 		imageLabel2.setIcon(new ImageIcon(img2));
 		imageLabel2.setBounds(110, 279, 46, 46);
@@ -85,6 +89,7 @@ public class LandingPage extends JFrame {
 		imageLabel3.setBounds(391, 279, 32, 40);
 		panel.add(imageLabel3);
 
+		// Login button setup and action listener
 		JButton buttonLogin = new JButton("Login");
 		buttonLogin.setBackground(Color.LIGHT_GRAY);
 		buttonLogin.setBounds(66, 330, 140, 40);
@@ -97,7 +102,11 @@ public class LandingPage extends JFrame {
 			}
 		});
 
+		// Register button setup and action listener
 		JButton buttonReg = new JButton("Register");
+		buttonReg.setBackground(Color.LIGHT_GRAY);
+		buttonReg.setBounds(336, 330, 140, 40);
+		panel.add(buttonReg);
 		buttonReg.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
@@ -106,12 +115,9 @@ public class LandingPage extends JFrame {
 				CloseFrame();
 			}
 		});
-		buttonReg.setBackground(Color.LIGHT_GRAY);
-		buttonReg.setBounds(336, 330, 140, 40);
-		panel.add(buttonReg);
-
 	}
 
+	// Frame closer function
 	public void CloseFrame() {
 		super.dispose();
 	}

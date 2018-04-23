a4e3cacd9f80be7810b1a4b0c9135b24d7e10920
Danielis Joniskis <g00333859@gmit.ie>
2018-04-23 20:58:32 +0100

    finished javadocs for Runner

--- Client/src/ie/gmit/proskills/Runner/Runner.java
+++ Client/src/ie/gmit/proskills/Runner/Runner.java
@@ -2,6 +2,16 @@ package ie.gmit.proskills.Runner;
 
 import ie.gmit.proskills.Menu.LandingPage;
 
+/**
+ * This class is the Runner class.<br>
+ * It simply runs the LandingPage main method
+ * and launches the menu<br>
+ *
+ * @author Cian Gannon
+ * @author Danielis Joniškis
+ * @author Eddie Eldridge
+ */
+
 //A Runner class with a main which runs the LandingPage class.
 public class Runner {
 

231b587cef269c1d3e82201a1f875bb8af3f2cf8
Cian Gannon <lego@live.ie>
2018-04-24 16:12:15 +0100

    Initial history setup

---
+++ DatabaseServer/src/ie/gmit/proskillsserver/proceeses/CreateHistory.java
@@ -0,0 +1,12 @@
+package ie.gmit.proskillsserver.proceeses;
+
+public class CreateHistory {
+
+	public static void main(String username) {
+		
+		
+		
+
+	}
+
+}
---
+++ DatabaseServer/src/ie/gmit/proskillsserver/proceeses/History.java
@@ -0,0 +1,10 @@
+package ie.gmit.proskillsserver.proceeses;
+
+public class History {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

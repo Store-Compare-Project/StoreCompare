63ed1c52889b0739a66e228b6540f83e74da053a
Cian Gannon <lego@live.ie>
2018-04-24 18:43:36 +0100

    Retreive history

--- DatabaseServer/src/ie/gmit/proskillsserver/proceeses/HistoryGet.java
+++ DatabaseServer/src/ie/gmit/proskillsserver/proceeses/HistoryGet.java
@@ -1,12 +1,45 @@
 package ie.gmit.proskillsserver.proceeses;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
 public class HistoryGet {
 
 	public static String main(String username) {
 		
 		String history = "";
 		
-		System.out.print("Test");
+		// MongoDB settings
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		DB db = mongoClient.getDB("history");
+		DBCollection coll = db.getCollection(username);
+		
+		// Gets all DB collections
+		DBCursor cursor = coll.find();
+		
+		try {
+			while (cursor.hasNext()) {
+				
+				// Get cursor items and adds them to the DBObject
+				DBObject temp = cursor.next();
+	
+				// Gets username and password
+				String itemTemp = temp.get("item").toString();
+				String priceTemp = temp.get("price").toString();
+				String dateTemp = temp.get("price").toString();
+				
+				history = history + itemTemp + " " + priceTemp + " " + dateTemp + " ";
+			}
+		} finally {
+			// Close cursor
+			cursor.close();
+
+			// Close database connection
+			mongoClient.close();
+		}
 		
 		return history;
 	}

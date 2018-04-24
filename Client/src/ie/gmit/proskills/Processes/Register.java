73a9a7646d4cfab1e98f77adfc683d32300b60e9
Danielis Joniskis <g00333859@gmit.ie>
2018-04-24 17:48:07 +0100

    added javadocs for Register

--- Client/src/ie/gmit/proskills/Processes/Register.java
+++ Client/src/ie/gmit/proskills/Processes/Register.java
@@ -3,7 +3,25 @@ package ie.gmit.proskills.Processes;
 import ie.gmit.proskills.object.LoginObject;
 import ie.gmit.proskills.serverconn.Requester;
 
+/**
+ * This class is responsible for registering the user into the system. <br>
+ * 
+ * @author Cian Gannon
+ * @author Danielis Joniškis
+ * @author Eddie Eldridge
+ */
+
 public class Register {
+	
+	/**
+	 * Method sends the username and password to the Requester.
+	 * 
+	 * @param username
+	 *            The username provided by the user
+	 * @param password
+	 *            The password provided by the user
+	 * @return returns the login details
+	 */
 
 	public static boolean main(String username, String password) {
 		

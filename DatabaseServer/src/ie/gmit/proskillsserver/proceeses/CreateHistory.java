a04d54da1548676bda188deeef53609e31dd747a
Cian Gannon <lego@live.ie>
2018-04-24 16:16:26 +0100

    Server history

--- DatabaseServer/src/ie/gmit/proskillsserver/proceeses/CreateHistory.java
+++ DatabaseServer/src/ie/gmit/proskillsserver/proceeses/CreateHistory.java
@@ -1,10 +1,17 @@
 package ie.gmit.proskillsserver.proceeses;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
 public class CreateHistory {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String username) {
 		
-		
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		DB db = mongoClient.getDB("history");
+		DBCollection coll = db.getCollection(username);
 		
 
 	}

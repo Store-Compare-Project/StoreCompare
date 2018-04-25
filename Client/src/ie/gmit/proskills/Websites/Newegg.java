091ec564a9834d6e0c27e4d47048822b03b2ef23
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-04-25 15:06:28 +0100

    Updated Newegg to add to the table

--- Client/src/ie/gmit/proskills/Websites/Newegg.java
+++ Client/src/ie/gmit/proskills/Websites/Newegg.java
@@ -1,26 +1,37 @@
 package ie.gmit.proskills.Websites;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.List;
+
+import javax.swing.table.DefaultTableModel;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import ie.gmit.proskills.Storage.Items;
+import ie.gmit.proskills.object.StoreInfo;
 
 public class Newegg {
 
 	// Run this class when we want to search amazon
-	public static void run(String searchTerm, List<Items> itemList) throws IOException
+	public static void run(String searchTerm, DefaultTableModel dtm) throws IOException
 	{
+		DecimalFormat df = new DecimalFormat("#.00");
+
 		// Varaibles
 		String name = null;
 		String priceString=null;
 		String urlPart1="https://www.newegg.com/global/ie/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=";
 		String urlPart2="&N=-1&isNodeId=1";
 		String completeUrl;
+		double postage = 7.50;
+		double total =0.00;
 		double price=0.00;
+		int totalQueries = 0;
+		double allPrices = 0;
 		
 		// Replace any spaces with a +
 		searchTerm = searchTerm.replaceAll(" ", "+");
@@ -42,31 +53,24 @@ public class Newegg {
 		// For every element of the element we assigned above
 		for(Element el : els)
 		{
-					try
-					{
-						name = (el.getElementsByClass("item-title").text()).replaceAll("Name: ", "");
-						//System.out.println(name);
-					} 
-					catch (Exception e)
-					{
-						System.out.println("Name not found.");
-					}
+			totalQueries++;
+				
+			name = (el.getElementsByClass("item-title").text()).replaceAll("Name: ", "");					
+												
+		    priceString =  ((el.getElementsByClass("price-current").text().replaceAll("[^0-9,]", "")));
+			String newString = priceString.replaceAll(",",".");
+			String subbedPriceString = newString.substring(0, newString.length() - 1);
+			price = Double.parseDouble(subbedPriceString);				
 					
-					try
-					{
-						priceString =  ((el.getElementsByClass("price-current").text().replaceAll("[^0-9,]", "")));
-						String newString = priceString.replaceAll(",",".");
-						String subbedPriceString = newString.substring(0, newString.length() - 1);
-						price = Double.parseDouble(subbedPriceString);
-					} 
-					catch (NumberFormatException e) 
-					{
-						continue;
-					}
-					
-					// Add the found stuff to our list
-					itemList.add(new Items(name, price));
-	}
+			total = price + postage;
+			
+			// Add the found stuff to our list
+			allPrices += total;
+			
+			dtm.addRow(new Object [] { name, price, postage, "€" +df.format(total), "Newegg"});
+		}
+		
+		StoreInfo.setNeweggAvg(df.format(allPrices/totalQueries));
 	}
 	
 }
--- Client/src/ie/gmit/proskills/object/StoreInfo.java
+++ Client/src/ie/gmit/proskills/object/StoreInfo.java
@@ -4,6 +4,7 @@ public class StoreInfo {
 
 	private static String ebayAVG = "";
 	private static String doneDealAVG = "";
+	private static String neweggAVG = "";
 
 	public static String getEbayAVG() {
 		
@@ -23,11 +24,24 @@ public class StoreInfo {
 		return doneDealAVG;
 	}
 
+	public static String getNeweggAVG() {
+		
+		if(neweggAVG.isEmpty()){
+			neweggAVG = "0.00";
+		}
+		
+		return neweggAVG;
+	}
+	
 	public static void setEbayAVG(String ebayAVGSet) {
 		ebayAVG = ebayAVGSet;
 	}
 
 	public static void setDoneDealAVG(String doneDealAVGSet) {
 		doneDealAVG = doneDealAVGSet;
+	}
+
+	public static void setNeweggAvg(String neweggAVGSet) {
+		neweggAVG = neweggAVGSet;		
 	}
 }
